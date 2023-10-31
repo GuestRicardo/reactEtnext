41983b8b82644f4f74a234304bd56984202ed73a
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-10-31 10:50:23 -0300

    formatando

--- projeto-1/src/App.js
+++ projeto-1/src/App.js
@@ -5,7 +5,21 @@ import { Component } from 'react';
 class App extends Component {
   state = {
     posts: [
-      
+     {
+      id: 1,
+      title: 'Titulo 1',
+      body: 'corpo 1',
+     },
+     {
+      id: 2,
+      title: 'Titulo 2',
+      body: 'corpo 2',
+     },
+     {
+      id: 3,
+      title: 'Titulo 3',
+      body: 'corpo 3',
+     }
     ]
   };
 
