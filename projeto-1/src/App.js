950e3b139b00b183cbea8172e6e6afa21359f173
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-06 15:53:58 -0300

    formatando

--- projeto-1/src/App.js
+++ projeto-1/src/App.js
@@ -6,34 +6,19 @@ class App extends Component {
   state = {
     counter: 0,
     posts: [
-      {
-        id: 1,
-        title: 'Titulo 1',
-        body: 'corpo 1',
-      },
-      {
-        id: 2,
-        title: 'Titulo 2',
-        body: 'corpo 2',
-      },
-      {
-        id: 3,
-        title: 'Titulo 3',
-        body: 'corpo 3',
-      }
+
     ]
   };
 
-  //aq esta disparando a função, e atualizara o estado da hudletimeout,
   // o metodo rendle sera chamado
   componentDidMount() {
 
   }
-  //aq esta atualizando o metodo infinitamente(aq no caso esta se chamando)
+
   componentDidUpdate() {
 
   }
-  //para limpar o lixo da pagina, quando o componente parar
+
   componentWillUnmount() {
 
   }
