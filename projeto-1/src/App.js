5a4318cf390c939c6ea170c601c9be2202e43279
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-07 15:25:07 -0300

    formatando

--- projeto-1/src/App.js
+++ projeto-1/src/App.js
@@ -12,24 +12,19 @@ class App extends Component {
 
   // o metodo rendle sera chamado
   componentDidMount() {
+
+  }
+  loadPosts = async () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())//convertendo em json
-    .then(posts => this.setState({posts}))//carregando
-  }
-
-  componentDidUpdate() {
-
-  }
-
-  componentWillUnmount() {
-
+      .then(response => response.json())//convertendo em json
+      .then(posts => this.setState({ posts }))//carregando
   }
 
   render() {
     const { posts } = this.state;
     return (
       <div className="App">
-        <h2>{}</h2>
+        <h2>{ }</h2>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
