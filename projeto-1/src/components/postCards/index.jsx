1896f80438a348a203b7293f566908c6164542c9
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-08 15:40:14 -0300

    formatando

---
+++ projeto-1/src/components/postCards/index.jsx
@@ -0,0 +1,7 @@
+<div className="post">
+  <img src={post.cover} alt={post.title} />
+  <div key={post.id} className='post-content'>
+    <h1>{post.title}</h1>
+    <p>{post.body}</p>
+  </div>
+</div>
