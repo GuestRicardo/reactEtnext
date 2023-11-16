b186d57e130d424e4089d9a6a2978f8c718ad029
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-16 14:48:16 -0300

    formatando

--- projeto-1/src/components/input/index.jsx
+++ projeto-1/src/components/input/index.jsx
@@ -1,11 +1,14 @@
 import { Component } from "react";
 import './style.css'
 
-export class InputSearch extends Component{
-    render(){
-        return(
-            <div className="busca">
-                <input type="search" name="" id="" />
+export class InputSearch extends Component {
+    render() {
+        return (
+            <div classNames="container">
+                <div className="input-group">
+                    <label className="input-group__label" for="myInput">My Label</label>
+                    <input type="text" id="myInput" class="input-group__input" value="This is my input" />
+                </div>
             </div>
         )
     }
