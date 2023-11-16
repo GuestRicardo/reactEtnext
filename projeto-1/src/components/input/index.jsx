d3a9cb959450be8bbe6faedf7f86cad7f4286d54
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-16 15:05:08 -0300

    formatando

--- projeto-1/src/components/input/index.jsx
+++ projeto-1/src/components/input/index.jsx
@@ -4,7 +4,7 @@ import './style.css'
 export class InputSearch extends Component {
     render() {
         return (
-            <div classNames="container">
+            <div classNames="containeri">
                 <div className="input-group">
                     <label className="input-group__label" for="myInput">My Label</label>
                     <input type="text" id="myInput" class="input-group__input" value="This is my input" />
